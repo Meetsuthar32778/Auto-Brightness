8c1193b6893ba48294800c78a2624dd23539cfd4
Meet Suthar <143948768+Meetsuthar32778@users.noreply.github.com>
2025-07-10 17:58:08 +0530

    Update Auto_Brightness.py
    adding keyboard shoutcut

--- Auto_Brightness.py
+++ Auto_Brightness.py
@@ -1,29 +1,35 @@
 import cv2
 import numpy as np
 import screen_brightness_control as sbc
-import time
+import keyboard
 
 def calculate_brightness(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return np.mean(gray)
 
+
+
 # Open the webcam
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_EXPOSURE,-5)
+cap.set(cv2.CAP_PROP_EXPOSURE,0)
+
+adj = 140
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    if keyboard.is_pressed("f5") and brightness-adj <=100:
+        print("+",brightness-adj)
+
+        adj-=2
+
+    if keyboard.is_pressed("f4") and brightness-adj >= 0:
+        adj+=2
+        print('-',brightness-adj)
     
     brightness = calculate_brightness(frame)
-    sbc.set_brightness(brightness)
-    time.sleep(1)
-
-    
-    # Break the loop on 'q' key press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    sbc.set_brightness(brightness-adj)
 
 cap.release()
 cv2.destroyAllWindows()
